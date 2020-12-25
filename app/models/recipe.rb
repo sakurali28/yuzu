df7067ac455e73503448772b9238ea970656d20d
YOUR NAME <YOUR@EMAIL.com>
2020-12-25 16:51:09 -0600

    adds cooktime conversion

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,4 +7,17 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_tags
   has_many :tags, through: :recipe_tags
+
+  def cooktime_conversion
+    if cooktime != nil
+      minutes = cooktime % 60
+      hours = cooktime / 60
+      time = ""
+      time += "#{hours} hour" if hours == 1
+      time += "#{hours} hours" if hours > 1
+      time += " #{minutes} minute" if minutes == 1
+      time += " #{minutes} minutes" if minutes > 1
+    end
+    time
+  end
 end
